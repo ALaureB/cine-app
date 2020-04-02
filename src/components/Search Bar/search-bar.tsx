69485353448f91736e7f7a3b41e5b0e9f07f7f7f
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2020-04-01 22:40:17 -0400

    Get value from search bar

--- src/components/Search Bar/search-bar.tsx
+++ src/components/Search Bar/search-bar.tsx
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+interface ISearchBarProps {}
 
 interface ISearchBarState {
+  searchText: string;
+  placeHolder: string;
 }
 
-class SearchBar extends Component<{}, ISearchBarState> {
+class SearchBar extends Component<ISearchBarProps, ISearchBarState> {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(props: Readonly<ISearchBarProps>) {
+    super(props);
+    this.state = {
+      searchText: "",
+      placeHolder: "Rechercher un film"
+    };
+  }
+
+  handleChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({
+      searchText: event.currentTarget.value
+    });
+  }
+
   render() {
-    return <input />;
+    return (
+      <input
+        placeholder={this.state.placeHolder}
+        onChange={this.handleChange.bind(this)}
+      />
+    );
   }
 }
 
